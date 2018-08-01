b465684403a19bec9233b49c4439631d54214326
BreakingBran <lancepereira99@gmail.com>
2018-07-31 23:58:03 -0400

    added tests for get \todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,8 +5,16 @@ const {app} = require('./../server');
 const {User} = require('./../models/user');
 const {Todo} = require('./../models/todo');
 
+todos = [{
+  text: 'First'
+}, {
+  text: 'Second'
+}]
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done());
 });
 
 describe('POST /todos', () => {
@@ -26,7 +34,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({text: text}).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -46,9 +54,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e));
       })
+  })
+})
+
+describe('GET /todos', () => {
+  it('should show the two seeded todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
   })
 })
