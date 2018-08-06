fa0250cf003d697ba2e118fca2d76e4b7209c80b
BreakingBran <lancepereira99@gmail.com>
2018-08-05 21:13:15 -0400

    Added tests for GET \todos\:_id

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {User} = require('./../models/user');
 const {Todo} = require('./../models/todo');
 
 todos = [{
+  _id: new ObjectID(),
   text: 'First'
 }, {
+  _id: new ObjectID(),
   text: 'Second'
 }]
 
@@ -70,5 +73,38 @@ describe('GET /todos', () => {
         expect(res.body.todos.length).toBe(2);
       })
       .end(done);
-  })
-})
+  });
+});
+
+describe('GET /todos/:_id', () => {
+
+  it('should be an invalid id', (done) => {
+    request(app)
+      .get('/todos/123')
+      .expect(400)
+      .expect((res) => {
+        expect(res.text).toBe("Id is not valid")
+      })
+      .end(done)
+  });
+
+  it('should not find the id', (done) => {
+    request(app)
+      .get(`/todos/${new ObjectID()}`)
+      .expect(404)
+      .expect((res) => {
+        expect(res.text).toBe("No Todo with that ID found")
+      })
+      .end(done)
+  });
+
+  it('should find the valid id', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text)
+      })
+      .end(done)
+  });
+});
